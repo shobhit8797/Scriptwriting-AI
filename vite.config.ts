c7166ec8811540cea13118fe6bb0d4e99e8d85be
shobhit8797 <shobhit8797@gmail.com>
2025-03-23 04:18:32 +0530

    should work now

--- vite.config.ts
+++ vite.config.ts
@@ -9,28 +9,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
+    plugins: [
+        react(),
+        runtimeErrorOverlay(),
+        themePlugin(),
+        ...(process.env.NODE_ENV !== "production" &&
+        process.env.REPL_ID !== undefined
+            ? [
+                  await import("@replit/vite-plugin-cartographer").then((m) =>
+                      m.cartographer()
+                  ),
+              ]
+            : []),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "client", "src"),
+            "@shared": path.resolve(__dirname, "shared"),
+        },
     },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+    root: path.resolve(__dirname, "client"),
+    build: {
+        outDir: path.resolve(__dirname, "server/public"),
+        emptyOutDir: true,
+    },
 });
